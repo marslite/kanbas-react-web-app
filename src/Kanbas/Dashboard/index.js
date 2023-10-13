0f387bf6f64c2daf6f60e6fb4cbd2d7880298034
menth0s <marslite@pm.me>
2023-10-13 03:12:45 -0400

    Implementing React Dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -2,6 +2,8 @@
 import {Link} from "react-router-dom";
 import db from "../Database";
 import KanbasNavigation from "../KanbasNavigation";
+import "./index.css"
+import courseImage from './course-bg.png'
 
 
 function Dashboard(){
@@ -11,12 +13,36 @@ function Dashboard(){
         <div>
 
             <h1>Dashboard</h1>
-            <div className="list-group">
-                {courses.map((course) => (<a  key={course._id} href={`/Kanbas/Courses/${course._id}`} className="list-group" >
+            <h4>Published Courses</h4>
+            <div className="containter-fluid flex-wrap moved">
+                <div className="row spacing">
+
+                    <div className="col white_spacing_up">
+                        <div className="card" style={{width: '270px'}}>
+                            <img className="card-img-top" src={courseImage}  alt="Card image cap"/>
+                            <div className="card-body"> 
+                            <h5 className="card-title"> CS4550 12631 Web Dev</h5>
+                            <h6>CS4550.12631</h6>
+                            <p className="card-text">17367_2 Summer 1 2023 .</p>
+                            <i className="fa fa-book" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                {/* {courses.map((course) => (<a  key={course._id} href={`/Kanbas/Courses/${course._id}`} className="list-group" >
                     {course.name}
                 </a>
-                ))}
+                ))} */}
+
+                </div>
+                
             </div>
+
+
         </div>
     );
 }
