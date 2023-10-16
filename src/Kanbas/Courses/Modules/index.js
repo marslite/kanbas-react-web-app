6d6d57a0ed09ceaafb86ea2cd60508fec7c8b85d
menth0s <marslite@pm.me>
2023-10-15 21:11:15 -0400

    Fixed the Breadcrump for all the pages

--- src/Kanbas/Courses/Modules/index.js
+++ src/Kanbas/Courses/Modules/index.js
@@ -1,9 +1,14 @@
 import ModuleList from "./ModuleList";
+import { useParams } from "react-router";
 
 function Modules(){
 
+    const path = useParams();
+    const here = console.log(path);
+
     return(
-    <div>
+    <div style={{marginTop: '47px'}}>
+
         <h2>Modules</h2>
         <ModuleList/>
     </div>
--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -4,23 +4,36 @@ import "./index.css"
 import { AiOutlineMenu } from "react-icons/ai";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
+import { Breadcrumb } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 
 
 function Courses(){
     const {courseId} = useParams();
+    const {pathname} = useLocation();
     const course = db.courses.find((course) => course._id === courseId);
-    // const here = console.log("inside MODULES");
+    const path = pathname.split('/');
+    console.log(path[4], "here");
+
 
     return(
         <div>
-            
-            <h4 className="spaced"> 
+            <div className="styled">
             <AiOutlineMenu className='courses-icon'/>
-                Course {course.name}
-                {/* <hr style="width:1300px;text-align:left;margin-left: -231px;">  */}
-                <hr style={{width: "1300px", textAlign: "left", marginLeft:"-231px"}}/>
 
-                </h4>
+            <Breadcrumb className="styled">
+            <ol className="breadcrumb " id="home_url">
+                <li className="breadcrumb-item spacing_nav "><a href="#">{course.name}</a></li>
+                <li className="breadcrumb-item active spacing_nav" aria-current="page">{path[4]}</li>
+                </ol>
+
+            </Breadcrumb>
+
+
+
+            <hr style={{width: "1300px", textAlign: "left", marginLeft:"-220px", marginTop:"3px"}}/>
+            </div>
+
             <CourseNavigation/>
 
             <div>
@@ -28,11 +41,11 @@ function Courses(){
                     <Routes>
                         //Routing need fixing
                         <Route path="/" element={<Navigate  to="Home" />} />
-                        <Route path="Home" element={<h1>Home </h1> } />
+                        <Route path="Home" element={<h1 className="home_s">Home  </h1> } />
                         <Route path="Modules" element={<Modules/>} />
-                        <Route path="Assignments" element={<h1>Assignments</h1>} />
-                        <Route  path="Assignments/:assignmentId" element={<h1>Assignments Editor</h1>}/>
-                        <Route path="Grades" element={<h1>Grades</h1>} />
+                        <Route path="Assignments" element={<h1 className="home_s">Assignments</h1>} />
+                        <Route  path="Assignments/:assignmentId" element={<h1 className="home_s">Assignments Editor</h1>}/>
+                        <Route path="Grades" element={<h1 className="home_s">Grades</h1>} />
                     </Routes>
 
                 </div>
