08e38e99596bb9e65872158b51e7661d601b8fdf
menth0s <marslite@pm.me>
2023-10-17 17:50:52 -0400

    Finalized Assignments

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,5 +1,13 @@
 import {Link, useParams} from "react-router-dom";
 import db from "../../Database";
+import {FaEllipsisVertical} from "react-icons/fa6"
+import {AiOutlinePlus} from "react-icons/ai"
+import {AiOutlineCheckCircle} from "react-icons/ai"
+import {MdArrowDropDown} from "react-icons/md"
+import {BsPlus} from "react-icons/bs"
+
+
+import "./index.css"
 
 
 function Assignments(){
@@ -9,16 +17,81 @@ function Assignments(){
     (assignment) => assignment.course === courseId );
 
     return(
-        <div style={{marginTop: '47px'}}>
+        <div style={{marginTop: '47px'}} className="container ">
             <h2>Assignments for course {courseId}</h2>
+            <div className="cont-edit container">
+                            <span className="inliner">
+                            <form className="d-flex " role="search">
+                            <input className="form-control me-2 sp2" type="search" placeholder="Search for Assignments" aria-label="Search"/>
+                          </form>
+                                <button className="btn btn-light spaced">
+                                <AiOutlinePlus className='plus-icon'/>
+                                    Group 
+                                </button>
+
+                                  <button className="btn btn-danger moduleBtn">
+                                  <AiOutlinePlus className='plus-icon'/>
+                                     Assignment
+                                    </button>
+                                  <button className="btn btn-light spaced">
+                                  <FaEllipsisVertical className='db-icon'/>
+
+                                  </button>
+
+                            </span>
+
+
+
+                    </div>
+
+                    <hr style={{width: "1000px", textAlign: "left", marginLeft:"-220px", marginTop:"3px"}}/>
+
+
+
+
+
+  
+
+
+        <div className="row">
+                <div className="col-12">
+
+            <li className="list-group-item list-group-item-secondary assid ">
+            <FaEllipsisVertical className='db-icon'/>
+            <FaEllipsisVertical className='db-icon double_Ellipsis'/>
+            <MdArrowDropDown className='db-icon'/>
+
+                    Assignments
+                    <div className="float-end">
+                    <button className="btn btn-light rou" >40% of Total</button>
+            <BsPlus className='db-icon'/>
+            <FaEllipsisVertical className='db-icon'/>
+                </div>
+            </li>   
+
             <div className="list-group">
+            {courseAssingments.map((assignment) => (
+            <a key={assignment._id} href={`#/kanbas/courses/${courseId}/${assignment._id}`}
+            className="list-group-item"
+            >
+                {assignment.title}</a>
+                    ))}
+                </div>
+            </div>
+        </div>
+
+
+
+            {/* <div className="list-group">
+
+
                 {courseAssingments.map((assignment) => (
                     <a key={assignment._id} href={`#/kanbas/courses/${courseId}/${assignment._id}`}
                     className="list-group-item"
                     >{assignment.title}</a>
                 ))}
 
-            </div>
+            </div> */}
         </div>
     );
 
