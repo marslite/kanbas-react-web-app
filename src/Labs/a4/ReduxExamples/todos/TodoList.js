033266171673366955d6bcc3b0da0c73cc823057
menth0s <marslite@pm.me>
2023-10-28 21:15:02 -0400

    Added todo

--- src/Labs/a4/ReduxExamples/todos/TodoList.js
+++ src/Labs/a4/ReduxExamples/todos/TodoList.js
@@ -5,12 +5,47 @@ function TodoList(){
         {id:"2", title: "Learn Node"}
     ]);
 
+    const [todo, setTodo] = useState({title: "Learn Mongo"});
+
+    const addTodo = (todo) => {
+        const newTodos = [...todos, {...todo, id: new Date().getTime.toString()}];
+        setTodos(newTodos);
+        setTodo({title: ""});
+    }
+
+    const deleteTodo = (id) => {
+        const newTodos = todos.filter((todo) => todo.id !== id );
+        setTodos(newTodos);
+    }
+
+    const updateTodo = (todo) => {
+        const newTodos = todos.map((item) => (item.id === todo.id ? todo : item) );
+        setTodos(newTodos);
+        setTodo({title: ""})
+    };
+
 
 
 
     return(
         <div>
             <h2>Todo List</h2>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <button className="btn btn-secondary" onClick={() => addTodo(todo)}> Add</button>
+                    <button className="btn btn-secondary" onClick={() => updateTodo(todo )}> Update</button>
+                    <input value={todo.title} onChange={(e) => setTodo({...todo, title: e.target.value})}/>
+                </li>
+                {todos.map((todo) => (
+                    <li key={todo.id} className="list-group-item">
+                        <button className="btn btn-secondary" onClick={() => {deleteTodo(todo.id)}}>Delete</button>
+                        <button className="btn btn-secondary" onClick={() => {setTodo(todo)}}>Edit</button>
+                        {todo.title}
+                    </li>
+                ) )}
+            </ul>
         </div>
     );
 }
+
+export default TodoList;
