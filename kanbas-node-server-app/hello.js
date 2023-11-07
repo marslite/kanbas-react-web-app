9f794cca32a1b7c640859bf77faf7de88372a7a9
menth0s <marslite@pm.me>
2023-11-07 01:41:20 -0500

    Implemented 3.1.1

--- kanbas-node-server-app/hello.js
+++ kanbas-node-server-app/hello.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// app.get('/hello', (req,res) => {res.send('Life is good!')});
+// app.get('/helen', (req,res) => {res.send('You owe me 50$!')});
+// app.get('/', (req,res) => {res.send('Welcome to FullStack Development!')});
+
+const Hello = (app) => {
+    app.get('/hello', (req,res) => {
+        res.send('Life is good!');
+    })
+
+
+
+    app.get('/helen', (req,res) => {
+        res.send("You owe me 50$");
+    })
+
+    app.get('/', (req,res) => {
+        res.send("Welcome to Full Stack Develepoment!")
+    })
+
+
+}
+
+export default Hello;
