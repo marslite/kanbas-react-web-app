e15a264e095eac490eeb7f5b6b9bc81df4de6fc1
menth0s <marslite@pm.me>
2023-11-14 16:35:58 -0500

    Implemented 4.3.2

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+
 
 // const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(cors());
+ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
--- kanbas-node-server-app/courses/routes.js
+++ kanbas-node-server-app/courses/routes.js
@@ -1,11 +1,23 @@
+// import App from "../../src/App.js";
 import Database from "../Database/index.js";
 
 function CourseRoutes(app){
+
+    app.get("/api/courses/:id", (req,res) => {
+        const {id} = req.params;
+        const course = Database.courses.find( (c) => c._id === id);
+        if(!course){
+            res.status(404).send("Course Not Found");
+            return;
+        }
+        res.send(course);
+    })
 
     app.put("/api/courses/:id", (req,res) => {
         //Implemented following Prof's Lecture
         const {id} = req.params;
         const course = req.body;
+        console.log(course, "Is this form here?")
         Database.courses = Database.courses.map( (c) => c._id === id ? {...course} : c);
         res.sendStatus(204)
     } )
