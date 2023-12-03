d9805f934d08ca638f2f6bb234a90541942f25bc
menth0s <marslite@pm.me>
2023-12-03 06:11:56 -0500

    making changes

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -8,20 +8,34 @@ import ModuleRoutes from "./modules/routes.js";
 import mongoose from "mongoose"
 import UserRoutes from "./users/routes.js"
 import cors from "cors";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
+
+// const CONNECTION_STRING = process.env.CONNECTION_STRING 
+
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+// || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(DB_CONNECTION_STRING)
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
 
 
 // const express = require('express');
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: process.env.FRONT_END
 }));
 const sessionOptions ={
     secret: "anything",
     resave: false,
     saveUninitialized: false,
 };
+if(process.env.NODE_ENV !== "development"){
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    }
+}
 app.use(session(sessionOptions))
 
 app.use(express.json());
--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -1,16 +1,18 @@
 import axios from "axios";
-export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+// export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+export const BASE_API = "https://kanbas-node-server-app-zi6l.onrender.com";
 // export const BASE_API = process.env.REACT_
 // export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 // export const BASE_API = "http://localhost:4000"
 export const USERS_API = `${BASE_API}/api/users`;
+
+const request = axios.create({withCredentials: true})
 
 export const signout = async () => {
     const response = request.post(`${USERS_API}/signout`);
     return response.data;
 }
 
-const request = axios.create({withCredentials: true})
 
 export const signup = async (credentials) => {
     const response = await request.post(`${USERS_API}/signup`, credentials);
